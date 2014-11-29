2085dea383c64f20296b9e89a8a7c4b8274b8163
Dave Hng <dave.hng@outlook.com>
2014-11-29 09:28:14 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+logadslstatus
+=============
+
+This is a simple script with a helper app to log the details of my ADSL connection over time when I had a problem with my telephone connection. I have a TP-Link 8817 modem in bridge mode hooked up to an Asus RT-N16 running Tomato.
+
+Dependencies:
+* cron (i'm running this script in linux periodically using cron)
+* wget 
+* mono (helper app is written in C#)
+* TP-Link 8817 modem
+
+The overall system flow is:
+
+Preparation:
+* Log into the router and use your browser's cookie inspector to list cookies for the router's IP address.
+* Find cookies C0 (hash of username) and C1 (hash of password), and copy their values.
+* Create a cookies.txt file containing these cookies and save it in the script directory.
+
+    # HTTP cookie file.
+    
+    [router ip address]	TRUE	/	FALSE	4102358400	C0	[C0 value]
+    [router ip address]	TRUE	/	FALSE	4102358400	C1	[C1 value]
+
+
+* Change the IP address in the modempage variable in getstats-tplink.sh to point to your modem.
+
+Running:
+* Run getstats-tplink.sh. This should log into the router, pull down the status page, call the helper app to scrape the connection details from it and append the scraped data as CSV to values.csv.
+* If that works, set up a cron job to run this at your desired interval (mine's running every minute).
+
---
+++ src/ExtractAdslInfo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ExtractAdslInfo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                return;
+            }
+
+            string filename = args[0];
+            string output = args[1];
+
+            //string filename = @"\\nyamo\starfox\adslstatus\stats-2014-11-28-16-51-32.html";
+            //string output = "c:\blah.txt";
+
+            var text = File.ReadAllText(filename);
+
+            var snr = FindValues("SNR Margin", text);
+            var lineAttenuation = FindValues("Line Attenuation", text);
+            var dataRate = FindValues("Data Rate", text);
+            var maxRate = FindValues("Max Rate", text);
+            var power = FindValues("POWER", text);
+
+            var toAppend = DateTime.Now.ToString("yyyy,MM,dd,HH,mm,ss,") + BuildCsv(snr, lineAttenuation, dataRate, maxRate, power) + Environment.NewLine;
+
+            File.AppendAllText(output, toAppend);
+        }
+
+        private static DownUpValues FindValues(string key, string text)
+        {
+            var position = text.IndexOf(key, System.StringComparison.Ordinal);
+
+            var values = new DownUpValues();
+
+            const string firstHit = @"</td><td width=""70"" align=center class=""tabdata"">";
+            var firstPositionStart = text.IndexOf(firstHit, position, System.StringComparison.Ordinal) + firstHit.Length;
+            var firstPositionEnd = text.IndexOf("</td>", firstPositionStart, System.StringComparison.Ordinal);
+            values.Downstream = text.Substring(firstPositionStart, firstPositionEnd - firstPositionStart).Trim();
+
+            const string secondHit = @"class=""tabdata"">";
+            var secondPositionStart = text.IndexOf(secondHit, firstPositionEnd, StringComparison.Ordinal) + secondHit.Length;
+            var secondPositionEnd = text.IndexOf("</td>", secondPositionStart, System.StringComparison.Ordinal);
+            values.Upstream = text.Substring(secondPositionStart, secondPositionEnd - secondPositionStart).Trim();
+
+            return values;
+        }
+
+        private static string BuildCsv(params DownUpValues[] values)
+        {
+            return string.Join(",",
+                values.ToList().ConvertAll(x => string.Format("{0},{1}", x.Downstream, x.Upstream)).ToArray());
+        }
+
+        private struct DownUpValues
+        {
+            public string Downstream { get; set; }
+            public string Upstream { get; set; }
+        }
+
+    }
+}
